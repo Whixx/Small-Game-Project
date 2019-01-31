62dd42df103121d4cb52f9338aa3f516b6235aa4
Filip Zachrisson <fjz345@hotmail.com>
2019-01-31 11:36:04 +0100

    Added player

---
+++ Solution/Player.cpp
@@ -0,0 +1,22 @@
+#include "Player.h"
+
+Player::Player(Mesh * mesh, Texture * texture) 
+	: Object(mesh, texture)
+{
+}
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+void Player::Update()
+{
+}
+
+void Player::Draw()
+{
+}
