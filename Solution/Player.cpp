f5bd56017ec20fad3d0e143c397aa9c14548cd89
FjZ345 <fjz345@hotmail.com>
2019-02-27 14:20:41 +0100

    Removed console spam

--- Solution/Player.cpp
+++ Solution/Player.cpp
@@ -374,14 +374,14 @@ void Player::Update(double dt)
 		this->boundingBoxHalfSize);
 
 #ifdef DEBUG
-	if (this->playerCamera.GetCameraPosition() != this->playerCamera.GetOldCameraPosition())
-	{
-		//printf("Map position: X:%.2f, Y:%.2f Playerheight:%.2f\n", playerCamera.GetCameraPosition().x, playerCamera.GetCameraPosition().z, playerCamera.GetCameraPosition().y);
-	
-		std::cout << "Forward Vector! X: " << this->playerCamera.GetForwardVector().x << std::endl;
-		std::cout << "Forward Vector! Y: " << this->playerCamera.GetForwardVector().y << std::endl;
-		std::cout << "Forward Vector! Z: " << this->playerCamera.GetForwardVector().z << std::endl;
-	}
+	//if (this->playerCamera.GetCameraPosition() != this->playerCamera.GetOldCameraPosition())
+	//{
+	//	//printf("Map position: X:%.2f, Y:%.2f Playerheight:%.2f\n", playerCamera.GetCameraPosition().x, playerCamera.GetCameraPosition().z, playerCamera.GetCameraPosition().y);
+	//
+	//	std::cout << "Forward Vector! X: " << this->playerCamera.GetForwardVector().x << std::endl;
+	//	std::cout << "Forward Vector! Y: " << this->playerCamera.GetForwardVector().y << std::endl;
+	//	std::cout << "Forward Vector! Z: " << this->playerCamera.GetForwardVector().z << std::endl;
+	//}
 	
 #endif
 
