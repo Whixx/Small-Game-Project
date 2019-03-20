2d5a2f6dee52cca7aa6e314c49cc372dc2c8c7e3
FjZ345 <fjz345@hotmail.com>
2019-03-20 14:49:08 +0100

    Joakim commentar fix

--- Solution/Player.cpp
+++ Solution/Player.cpp
@@ -614,16 +614,6 @@ void Player::PlayAmbientSound()
 	this->ambientSound.Play();
 }
 
-void Player::StopAmbientSound()
-{
-	this->ambientSound.Stop();
-}
-
-void Player::PlayAmbientSound()
-{
-	this->ambientSound.Play();
-}
-
 void Player::AddCoinToWorld(unsigned int state)
 {
 	// Check if the player got coins to drop/toss
